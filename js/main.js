2c8f3f6eca39e0e0caad31ee851864dc0a661feb
Cristhian Forero <cristhianforerod@gmail.com>
2020-09-07 01:32:32 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const URL = 'https://randomuser.me/api/?results=18';
+
+let users = [];
+
+const drawContainer = () => {
+  const container = document.querySelector('#container');
+  document.querySelector('#btn').innerHTML = 'Generar nuevos';
+  container.innerHTML = '';
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then(({ results }) => {
+      results.forEach((user) => {
+        const { name, picture, login } = user;
+        users = [...users, user];
+
+        const div = document.createElement('div');
+        div.className = '';
+
+        div.innerHTML = `
+          <a href="javascript:void(0)" class="container-user">
+            <figure class="container-user__image">
+              <img src=${picture.large} alt='Photo user'/>
+            </figure>
+            <span class="container-user--overlay" uuid="${login.uuid}">
+              <p>
+                <span class="container-user--first" uuid="${login.uuid}">${name.first}</span>
+                <span class="container-user--last" uuid="${login.uuid}">${name.last}</span>
+              </p>
+            </span>
+          </a>
+        `;
+
+        container.appendChild(div);
+        container.addEventListener('click', userDetail);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      drawContainer();
+    });
+};
+
+document.addEventListener('DOMContentLoaded', (event) => drawContainer());
+
+const userDetail = (event) => {
+  document.querySelector('#container').style.display = 'none';
+  document.querySelector('#btn').innerHTML = 'Regresar';
+  const uuid = event.target.getAttribute('uuid');
+  const user = getUser(uuid);
+  const detailContainer = document.querySelector('#detail');
+  const div = document.createElement('div');
+  div.className = 'frame';
+  div.innerHTML = `
+    <div class="detail-container">
+      <img src=${user.picture.large} class="detail-container__image"/>
+      <div class="detail-container__description">
+        <span><span class="attribute">Nombre:</span> ${user.name.first} ${user.name.last} </span>
+        <span><span class="attribute">Email:</span> ${user.email}</span>
+        <span><span class="attribute">Dirección:</span> ${user.location.city},  ${user.location.street.number} ${user.location.street.name}</span>
+        <span><span class="attribute">Número télefonico:</span> ${user.phone}</span>
+        <span><span class="attribute">Número celular:</span> ${user.cell}</span>
+      </div>
+    </div>
+  `;
+  detailContainer.appendChild(div);
+};
+
+const getUser = (uuid) => {
+  const index = users.findIndex((u) => u.login.uuid === uuid);
+  return users[index];
+};
+
+document.querySelector('#btn').addEventListener('click', () => {
+  document.querySelector('#container').style.display = 'grid';
+  document.querySelector('#detail').classList.toggle('frame');
+  users = [];
+  drawContainer();
+});
